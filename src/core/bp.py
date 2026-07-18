042d6f9359089fd6cc85a68246ca81bd61ffd06b
Unbo10 <srochap@unal.edu.co>
2026-07-18 18:32:27 -0500

    feat(bp): intra-parallelization of BP
    Replaced a for that made possible a stage pass or step for multiple np array operations

--- src/core/bp.py
+++ src/core/bp.py
@@ -6,6 +6,7 @@ from .encoder import PolarEncoder, PolarCode
 from .generator import _bit_reverse
 
 def _int_to_bits(value: int, num_bits: int) -> np.ndarray:
+    # print(type(value), type(num_bits))
     return np.array([(value >> i) & 1 for i in range(num_bits - 1, -1, -1)], dtype=int)
 
 def _bit_reverse_perm(N: int) -> list[int]:
@@ -19,12 +20,13 @@ def _bit_reverse_perm(N: int) -> list[int]:
 
 def _build_bit_indices(N: int) -> list[np.ndarray]:
     indices: list[np.ndarray] = []
+    n_PEs: int = int(np.log2(N))
     for idx in range(N):
-        indices.append(_int_to_bits(idx, int(np.log2(N))))
+        indices.append(_int_to_bits(idx, n_PEs))
     return indices
 
 def _boxplus(x: float, y: float) -> float:
-    return np.sign(x) * np.sign(y) * min(np.abs(x), np.abs(y))
+    return np.sign(x) * np.sign(y) * np.minimum(np.abs(x), np.abs(y))
 
 def _build_graph(N: int, bit_indices: list[np.ndarray] | None = None) -> List[List[Tuple[str, int]]]:
     """
@@ -107,6 +109,29 @@ def _initialize_L(N: int, llr_1N: np.ndarray, perm: list[int]) -> np.ndarray:
     L[:, n_cols - 1] = llr_1N[perm]
     return L
 
+def _build_graph_arrays(graph: List[List[Tuple[str, int]]]) -> Tuple[List[np.ndarray]]:
+    """
+    Returns two lists:
+    - Each element of the first one is an array with the partner of each
+    corresponding node (arrs of partners)
+    - Each element of the second one is an array with type of node each
+    corresponding node is (arrs of types, i.e., '+' or '=').
+    """
+    graph_partners: List[np.ndarray] = []
+    graph_types: List[np.ndarray] = []
+    for j in range(len(graph[0])):
+        graph_partners.append([])
+        graph_types.append([])
+        for bit_pos in graph:
+            graph_partners[j].append(bit_pos[j][1])
+            graph_types[j].append(bit_pos[j][0])
+
+    for j in range(len(graph_partners)):
+        graph_partners[j] = np.asarray(graph_partners[j], dtype=np.int64)
+        graph_types[j] = np.asarray(graph_types[j], dtype=np.str_)
+
+    return graph_partners, graph_types
+
 def _right_sweep(R: np.ndarray, L: np.ndarray, graph: List[List[Tuple[str, int]]]) -> np.ndarray:
     """
     Left-to-right pass of u's estimate through the polarized channel's graph.
@@ -114,13 +139,25 @@ def _right_sweep(R: np.ndarray, L: np.ndarray, graph: List[List[Tuple[str, int]]
     for n_stage in range(1, len(R[0])):
         for node_i in range(len(R)):
             node_j: int = graph[node_i][n_stage-1][1] #*partner of node i
-            # import pdb; pdb.set_trace()
             if graph[node_i][n_stage-1][0] == "+": #*the -1 because it has log2(N) - 1 cols
                 R[node_i][n_stage] = _boxplus(R[node_i][n_stage-1],
                                               L[node_j][n_stage] + R[node_j][n_stage-1])
             else:
                 R[node_i][n_stage] = _boxplus(R[node_j][n_stage-1], L[node_j][n_stage]) \
                                      + R[node_i][n_stage-1]
+    return R
+
+def _right_sweep_parallel(R: np.ndarray, L: np.ndarray, partner_arrs: List[np.ndarray], type_arrs: List[np.ndarray]) -> np.ndarray:
+    """
+    Left-to-right pass of u's estimate through the polarized channel's graph
+    using parallelization to traverse all paths simulteanously.
+    """
+    for n_stage in range(1, len(R[0])):
+        nodes_j: np.ndarray = partner_arrs[n_stage-1]
+        nodes_j_types: np.ndarray = type_arrs[n_stage-1]
+        R[:,n_stage] = np.where(nodes_j_types == "+",
+                                 _boxplus(R[:,n_stage-1], L[nodes_j,n_stage] + R[nodes_j,n_stage-1]), # +
+                                 _boxplus(R[nodes_j,n_stage-1], L[nodes_j,n_stage]) + R[:,n_stage-1]) # =
     return R
 
 def _left_sweep(R: np.ndarray, L: np.ndarray, graph: List[List[Tuple[str, int]]]) -> np.ndarray:
@@ -133,13 +170,27 @@ def _left_sweep(R: np.ndarray, L: np.ndarray, graph: List[List[Tuple[str, int]]]
             node_j: int = graph[node_i][n_stage-1][1]
             if graph[node_i][n_stage-1][0] == "+":
                 L[node_i][n_stage-1] = _boxplus(L[node_i][n_stage],
-                                                L[node_j][n_stage] + R[node_j][n_stage-1] )
+                                                L[node_j][n_stage] + R[node_j][n_stage-1])
             else:
                 L[node_i][n_stage-1] = _boxplus(R[node_j][n_stage-1] , L[node_j][n_stage]) \
                                      + L[node_i][n_stage]
     return L
 
-def bp_decode(frozen_bits: frozenset, llr_1N: np.ndarray, iter_cap: int = 1000) -> np.ndarray:
+def _left_sweep_parallel(R: np.ndarray, L: np.ndarray, partner_arrs: List[np.ndarray], type_arrs: List[np.ndarray]) -> np.ndarray:
+    """
+    Right-to-left pass of L's last column (initially the LLRs) through the
+    polarized channel's graph using parallelization to traverse all paths
+    simulteanously.
+    """
+    for n_stage in range(len(L[0]) - 1, 0, -1):
+        nodes_j_types: np.ndarray = type_arrs[n_stage-1]
+        nodes_j: np.ndarray = partner_arrs[n_stage-1]
+        L[:,n_stage-1] = np.where(nodes_j_types == "+",
+                                _boxplus(L[:,n_stage], L[nodes_j,n_stage] + R[nodes_j,n_stage-1]),
+                                _boxplus(R[nodes_j,n_stage-1] , L[nodes_j,n_stage]) + L[:,n_stage])
+    return L
+
+def bp_decode(frozen_bits: frozenset, llr_1N: np.ndarray, iter_cap: int = 1000, parallel: bool = False) -> np.ndarray:
     i: int = 0
     u: list[int] = []
     N: int = llr_1N.size
@@ -149,6 +200,8 @@ def bp_decode(frozen_bits: frozenset, llr_1N: np.ndarray, iter_cap: int = 1000) 
     perm: list[int] = _bit_reverse_perm(N)
     R: np.ndarray = _initialize_R(N, frozen_bits)
     L: np.ndarray = _initialize_L(N, llr_1N, perm)
+    if parallel:
+        part_arrs, type_arrs = _build_graph_arrays(graph)
     # print("initial L:")
     # for row in L:
     #     print(row)
@@ -160,8 +213,12 @@ def bp_decode(frozen_bits: frozenset, llr_1N: np.ndarray, iter_cap: int = 1000) 
     encoder = PolarEncoder(PolarCode(N=N, K=K, frozen_positions=frozen_bits))
     # print(N, K)
     while i < iter_cap:
-        R = _right_sweep(R, L, graph)
-        L = _left_sweep(R, L, graph)
+        if not parallel:
+            R = _right_sweep(R, L, graph)
+            L = _left_sweep(R, L, graph)
+        else:
+            R = _right_sweep_parallel(R, L, part_arrs, type_arrs)
+            L = _left_sweep_parallel(R, L, part_arrs, type_arrs)
         u_belief: np.ndarray = R[:,0] + L[:,0] #*taken from the sweeps of *LLRs*
         u_hat_lst: list[int] = []
         for j, u_i in enumerate(u_belief):
